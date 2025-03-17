2b743061b6850eab83d4c953ff123a6572ff878a
g-jaffe <gjaffe@tacc.utexas.edu>
2025-03-17 12:23:20 -0400

    added TACC logo to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,8 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+# TACC logo
+html_logo = 'images/TACC-White-No-Mask.png'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
